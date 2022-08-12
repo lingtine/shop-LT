9f84f8c1a79c74190451ee2a1b86caadc522d275
lingtine <lingtine@gmail.com>
2022-08-12 21:23:57 +0700

    make header

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,4 +1,5 @@
 import './GlobalStyles.scss';
+import './grid.scss';
 
 function GlobalStyles({ children }) {
     return children;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,45 @@
+import styles from './header.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>heading</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('top-bar')}>
+                <p>
+                    Hotline Mua Hàng: 0973 285 886 | Hotline CSKH: 1900 886 803
+                    - Ext 1 | Email CSKH: web@360boutique.vn
+                </p>
+            </div>
+            <div className={cx('header-main')}>
+                <div className={cx('header-mid')}>
+                    <img
+                        className={cx('header-logo')}
+                        src={
+                            'https://360boutique.vn/wp-content/uploads/2021/10/LOGO-360-DUNG-TAM-THOI-MAU-DEN-05.png'
+                        }
+                        alt={'logo'}
+                    />
+                    <div className={cx('header-search')}>
+                        <input
+                            className={cx('header-input')}
+                            placeholder="Tìm kiếm sản phẩm"
+                        ></input>
+                        <button className={cx('header-search--icon')}>
+                            <FontAwesomeIcon
+                                icon={faMagnifyingGlass}
+                            ></FontAwesomeIcon>
+                        </button>
+                    </div>
+                    <div className={cx('action')}>action</div>
+                </div>
+                <div className={cx('header-menu')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
