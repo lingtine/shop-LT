03387f2bf718de8097fa9fd644c49b051001046a
lingtine <lingtine@gmail.com>
2022-09-01 18:16:38 +0700

    make component ProductItem

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,42 +4,44 @@ import { Link } from 'react-router-dom';
 import { forwardRef } from 'react';
 const cx = classNames.bind(styles);
 
-function Button(
-    {
-        href,
-        to,
-        children,
-        primary,
-        className,
-        leftIcon,
-        rightIcon,
-        ...passProps
+const Button = forwardRef(
+    (
+        {
+            href,
+            to,
+            children,
+            primary,
+            className,
+            leftIcon,
+            rightIcon,
+            ...passProps
+        },
+        ref,
+    ) => {
+        let Comp = 'button';
+
+        const classes = cx('wrapper', {
+            primary,
+            [className]: className,
+        });
+
+        const props = {};
+
+        if (to) {
+            Comp = Link;
+            props.to = to;
+        } else if (href) {
+            Comp = 'a';
+            props.href = href;
+        }
+        return (
+            <Comp className={classes} {...props} ref={ref} {...passProps}>
+                <span className={cx('icon')}>{leftIcon}</span>
+                <span className={cx('content')}>{children}</span>
+                <span className={cx('icon')}>{rightIcon}</span>
+            </Comp>
+        );
     },
-    ref,
-) {
-    let Comp = 'button';
+);
 
-    const classes = cx('wrapper', {
-        primary,
-        [className]: className,
-    });
-
-    const props = {};
-
-    if (to) {
-        Comp = Link;
-        props.to = to;
-    } else if (href) {
-        Comp = 'a';
-        props.href = href;
-    }
-    return (
-        <Comp className={classes} {...props} ref={ref} {...passProps}>
-            <span className={cx('icon')}>{leftIcon}</span>
-            <span className={cx('content')}>{children}</span>
-            <span className={cx('icon')}>{rightIcon}</span>
-        </Comp>
-    );
-}
-
-export default forwardRef(Button);
+export default Button;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './Header/index';
 export { default as Footer } from './Footer/index';
 export { default as Banner } from './Banner/index';
+export { default as ProductItem } from './ProductItem/index';
+export { default as Button } from './Button/index';
